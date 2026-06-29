8e62495b06d5b8fd4881ffd7c0730c25628d3236
dvaustria741-a11y <dvaustria741@gmail.com>
2026-06-29 17:35:08 +0800

    fix: KeyBinding.Category now takes Identifier in 1.21.11

--- src/main/java/com/auraclient/AuraClient.java
+++ src/main/java/com/auraclient/AuraClient.java
@@ -8,6 +8,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +30,7 @@ public class AuraClient implements ClientModInitializer {
                 "key.auraclient.opengui",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_RIGHT_SHIFT,
-                new KeyBinding.Category("category.auraclient")
+                new KeyBinding.Category(Identifier.of("auraclient", "category"))
         ));
 
         ModuleManager.init();
