243f394a3cdb4ff851b22610dfa594d0407e4167
dvaustria741-a11y <dvaustria741@gmail.com>
2026-07-07 15:51:05 +0800

    fix: remove RenderSystem dep - use fill overlay for transparency instead

--- src/main/java/com/azeluxclient/gui/AzeluxClickGui.java
+++ src/main/java/com/azeluxclient/gui/AzeluxClickGui.java
@@ -5,7 +5,6 @@ import com.azeluxclient.module.ModuleManager;
 import com.azeluxclient.setting.BooleanSetting;
 import com.azeluxclient.setting.Setting;
 import com.azeluxclient.setting.SliderSetting;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
@@ -102,10 +101,9 @@ public class AzeluxClickGui extends Screen {
     public void render(DrawContext ctx, int mx, int my, float delta) {
         ctx.fill(0, 0, width, height, 0x55000000);
 
-        // Draw background.png semi-transparently for translucent dark-glass effect
-        RenderSystem.setShaderColor(1f, 1f, 1f, 0.80f);
+        // Draw background.png at full opacity, then overlay a subtle dark tint for translucency
         texScaled(ctx, T_BG, panX, panY, panW, panH, 1072, 658);
-        RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+        ctx.fill(panX, panY, panX + panW, panY + panH, 0x33000000);
 
         ctx.fill(panX + sideW, panY + navH + 4, panX + sideW + 1, panY + panH - 4, 0x33FFFFFF);
 
@@ -134,9 +132,9 @@ public class AzeluxClickGui extends Screen {
             int tw = tabWidths[i];
             boolean sel = (activeTab == i);
             // Semi-transparent tab buttons: selected slightly more opaque
-            RenderSystem.setShaderColor(1f, 1f, 1f, sel ? 0.85f : 0.50f);
             texScaled(ctx, sel ? T_BTN_DEFAULT : T_BTN_LESS, tabX, tabY, tw, tabH, 249, 249);
-            RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+            // Semi-transparent overlay on unselected tabs
+            if (!sel) ctx.fill(tabX, tabY, tabX + tw, tabY + tabH, 0x66000000);
             ctx.drawText(textRenderer, TABS[i],
                     tabX + (tw - textRenderer.getWidth(TABS[i])) / 2,
                     tabY + tabH / 2 - 4,
